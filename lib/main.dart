3453aa9b89ae17e6a0c6925f3bc4bbc96238e687
amahrou1 <abdalah336@gmail.com>
2021-03-19 02:01:07 -0700

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:module/info.dart';
-// import 'info.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,42 +8,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  final Color b = Colors.black;
-  final Color w = Colors.white;
-
-  final List<Info> li = [
-    Info(name: 'ali 1', height: 170, date: DateTime.now()),
-    Info(name: 'ali 2', height: 171, date: DateTime.now()),
-    Info(name: 'ali 3', height: 172, date: DateTime.now()),
-    Info(name: 'ali 4', height: 173, date: DateTime.now()),
-    Info(name: 'ali 5', height: 174, date: DateTime.now())
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            ...li.map((val) {
-              return Row(
-                children: [
-                  val.name != "ali 3" ? Text(val.name) : Text("done"),
-                  Text('${val.height}'),
-                  Text('${val.date}'),
-                ],
-              );
-            }).toList(),
+            myBigContainer(),
+            myListTeil(
+              iconLeading: Icons.language_outlined,
+              text: Text('Chang To Arabic'),
+            ),
+            myBigContainer(),
+            myListTeil(
+                iconLeading: Icons.settings_applications_outlined,
+                text: Text('Account Settings')),
+            myBigContainer(),
+            myListTeil(
+                iconLeading: Icons.how_to_reg_outlined,
+                text: Text('HowTo Use')),
+            mySmallContainer(),
+            myListTeil(
+                iconLeading: Icons.send_to_mobile,
+                text: Text('Send To Friend')),
+            mySmallContainer(),
+            myListTeil(
+                iconLeading: Icons.recommend, text: Text('Recommend a Store')),
+            myBigContainer(),
+            myListTeil(
+                iconLeading: Icons.add_business_outlined,
+                text: Text('Add Your Buisness')),
+            myBigContainer(),
+            myListTeil(
+                iconLeading: Icons.support_outlined,
+                text: Text('Karmalk Support')),
+            mySmallContainer(),
+            myListTeil(
+              iconLeading: Icons.save,
+              text: Text('Terms of Seervices'),
+            ),
+            mySmallContainer(),
+            myListTeil(
+                iconLeading: Icons.privacy_tip_outlined,
+                text: Text('Privacy policy')),
+            myBigContainer(),
+            myListTeil(
+                iconLeading: Icons.logout,
+                text: Text('Log Out'),
+                isVisibel: false)
           ],
         ),
       ),
+    );
+  }
+
+  ListTile myListTeil({
+    @required IconData iconLeading,
+    @required Widget text,
+    bool isVisibel = true,
+  }) {
+    return ListTile(
+      leading: Icon(iconLeading),
+      title: text,
+      trailing:
+          Visibility(visible: isVisibel, child: Icon(Icons.arrow_forward_ios)),
+      onTap: () {},
+    );
+  }
+
+  Container myBigContainer() {
+    return Container(
+      color: Colors.grey[300],
+      height: 35,
+    );
+  }
+
+  Padding mySmallContainer() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 22),
+      child: Container(
+          color: Colors.grey[300],
+          height: 2,
+          padding: EdgeInsets.symmetric(horizontal: 50)),
     );
   }
 }
